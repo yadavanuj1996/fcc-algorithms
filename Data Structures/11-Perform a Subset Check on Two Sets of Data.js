2ee151c88d1002e51b3b22f19dd231d08c5835a9
yadavanuj1996 <yadavanuj@hotmail.com>
2019-09-11 23:48:17 +0530

    Add subset check solution.

---
+++ Data Structures/11-Perform a Subset Check on Two Sets of Data.js
@@ -0,0 +1,76 @@
+class Set {
+    constructor(){
+        this.collection = [];
+    }
+
+    has(element){
+        return ( this.collection.indexOf(element) !== -1);
+    }
+
+    values(){
+        return  this.collection;
+    }
+ 
+    add(element){
+        if(!this.has(element)){
+             this.collection.push(element);
+            return true;
+        }
+        return false;
+    }
+ 
+   remove(element){
+        if(this.has(element)){
+           var index = this.collection.indexOf(element);
+            this.collection.splice(index,1);
+            return true;
+        }
+        return false;
+    }
+ 
+   size(){
+        return this.collection.length;
+    }
+
+    union(secondSet){
+        let unionSet=new Set();
+        this.collection.forEach((x)=>{
+            unionSet.add(x);
+        });
+
+        secondSet.collection.forEach((x)=>{
+            unionSet.add(x);
+        });
+        return unionSet;
+    }
+    intersection(secondSet){
+        let intersectionSet=new Set();
+        this.collection.forEach((x)=>{
+            if(!intersectionSet.has(x) && secondSet.has(x)){
+                intersectionSet.add(x);
+            }
+        });
+        return intersectionSet;
+    }
+    difference(secondSet){
+        let differenceSet=new Set();
+        this.collection.forEach((x)=>{
+            if(!secondSet.has(x)){
+                differenceSet.add(x);
+            }
+        });
+        return differenceSet;
+
+    }
+    subset(secondSet){
+        let isFirstSetSubset=true;
+        this.collection.forEach((x)=>{
+            if(!secondSet.has(x)){
+                isFirstSetSubset=false;
+            }
+        });
+       return isFirstSetSubset;
+    }
+}
+
+
