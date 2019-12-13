b7418ff71bc369c37fec2a37d7f49b482bfa18a8
yadavanuj1996 <yadavanuj@hotmail.com>
2019-12-13 23:25:29 +0530

    Add code for Find Min and Max in BST.

---
+++ Data Structures/28-Find the Minimum and Maximum Value in a Binary Search Tree.js
@@ -0,0 +1,98 @@
+
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
+  this.getLeftChild=function(){
+    return this.left;
+  }
+  this.getRightChild=function(){
+    return this.right;
+  }
+  this.hasNoLeftChild=function(){
+    return this.left === null; 
+  }
+
+  this.hasNoRightChild=function(){
+    return this.right === null; 
+  }
+
+  this.addLeftChild=function(element){
+    this.left=new Node(element);
+  }
+  
+  this.addRightChild=function(element){
+    this.right=new Node(element);
+  }
+  this.isGreaterThan=function(element){
+    return this.value > element;
+  }
+  this.isLessThan=function(element){
+    return this.value < element;
+  }
+}
+
+function BinarySearchTree() {
+  this.root = null;
+
+  this.add=function(element){
+    if(this.isEmpty()){
+      this.root=new Node(element);
+      return ;
+    }
+    let currentNode=this.root;
+   
+    while(currentNode !== null){
+      if(currentNode.isGreaterThan(element)){
+        if(currentNode.hasNoLeftChild()){
+          currentNode.addLeftChild(element);
+          return ;
+        }
+        currentNode=currentNode.getLeftChild(); 
+      }
+      else if(currentNode.isLessThan(element)){
+        if(currentNode.hasNoRightChild()){
+          currentNode.addRightChild(element);
+          return ;
+        }
+
+        currentNode=currentNode.getRightChild(); 
+      }
+      else{
+        return null;
+      }
+    }
+  }
+  
+  this.findMin=function(){
+    if(this.isEmpty())
+      return null;
+
+    let currentNode=this.root;
+    while(currentNode.getLeftChild() !== null){
+      currentNode=currentNode.getLeftChild();
+    }
+    return currentNode.value;
+  }
+
+  this.findMax=function(){
+    if(this.isEmpty())
+      return null;
+
+    let currentNode=this.root;
+    while(currentNode.getRightChild() !== null){
+      currentNode=currentNode.getRightChild();
+    }
+    return currentNode.value;
+  }
+
+  this.isEmpty=function(){
+    return this.root === null
+  }
+}
+
+
+
